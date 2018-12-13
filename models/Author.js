cb01f94813abd98c83e3c263bff35f8faf245075
javieramirez90 <javier.ramirez@aiesec.net>
2018-12-13 10:15:22 -0600

    first commit

---
+++ models/Author.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// const Book = require('../models/Book');
+
+const authorSchema = new Schema({
+  name: String,
+  nationallity: String,
+  books: [
+    {type: Schema.Types.ObjectId,
+    ref: 'Book'}
+  ]
+},{
+  timestamps:{
+    createdAt: true,
+    updatedAt: true
+  }
+});
+
+module.exports = mongoose.model('Author', authorSchema)
---
+++ routes/authors.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const Author = require('../models/Author');
+
+const router = express.Router();
+
+//C
+//(Create new author)
+router.post('/authors', (req, res, next) => {
+  Author.create({
+    name: req.body.name,
+    nationallity: req.body.nationallity,
+    books: []
+  })
+  .then(response=>{
+    res.json(response)
+  })
+  .catch(e=>res.json(e))
+})
+
+//R
+//(Get all authors )
+router.get('/authors', (req, res, next) =>{
+  Author.find()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(e=> res.json(e))
+})
+//(Get single author by id)
+router.get('/authors/:id', (req, res, next) =>{
+  Author.findById(req.params.id)
+    .then(response => {
+      res.json(response);
+    })
+    .catch(e=> res.json(e))
+})
+//U
+//(Update name or nat)*
+
+//D
+//(Delete by id)
+
+module.exports = router;
