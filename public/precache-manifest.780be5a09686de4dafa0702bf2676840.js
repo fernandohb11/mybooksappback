94f2b5eb4ab35f76107826703c98542ab3afff96
Fernando Hernandez <fernandohernandez@MacBook-Pro-de-Fernando.local>
2019-01-08 18:08:14 -0600

    first deploy

---
+++ public/precache-manifest.780be5a09686de4dafa0702bf2676840.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "13bfd7636e93f26dcb0aac017e88bbc5",
+    "url": "/static/media/home1.13bfd763.jpg"
+  },
+  {
+    "revision": "6c446229399caa0d42b5",
+    "url": "/static/css/main.c1c7dbb7.chunk.css"
+  },
+  {
+    "revision": "924e6c69836a90f6f72a7071a93dde1a",
+    "url": "/static/media/home6.924e6c69.jpg"
+  },
+  {
+    "revision": "11fa9d28c32eb1dfcf4d",
+    "url": "/static/js/1.11fa9d28.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1e724b9c23d7b89384a382d17fcaa6d",
+    "url": "/static/media/banner.f1e724b9.png"
+  },
+  {
+    "revision": "24aa1203af9b94337657e3ea038aebea",
+    "url": "/static/media/pasosBannerFull.24aa1203.png"
+  },
+  {
+    "revision": "6c446229399caa0d42b5",
+    "url": "/static/js/main.6c446229.chunk.js"
+  },
+  {
+    "revision": "f90e55c42f0c16334e1496e86cadc032",
+    "url": "/static/media/home2.f90e55c4.jpg"
+  },
+  {
+    "revision": "d16e955420091a52a489738033f2643f",
+    "url": "/static/media/home3.d16e9554.jpg"
+  },
+  {
+    "revision": "c75e270ab647aa140e41fe42c637da15",
+    "url": "/static/media/home4.c75e270a.jpg"
+  },
+  {
+    "revision": "258007b741ddc7ed6dc41bed3f2c1a2f",
+    "url": "/static/media/home5.258007b7.jpg"
+  },
+  {
+    "revision": "11fa9d28c32eb1dfcf4d",
+    "url": "/static/css/1.c2223f19.chunk.css"
+  },
+  {
+    "revision": "a7013265db8be881a8d1a7f853561c4b",
+    "url": "/index.html"
+  }
+];
